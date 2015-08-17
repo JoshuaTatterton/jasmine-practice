8a7172885baaa9a6ca2a208446c473ce7e136b91
RichardCharman <richardcharman@live.com>
2015-08-17 12:22:08 +0000

    Added additional spec tests

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,15 +1,34 @@
 describe("Javabuzz", function() {
   var javabuzz;
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
   describe("knows when a number is", function() {
     it("divisible by three", function () {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
-    })
-  })
+    });
+        it("divisible by five", function () {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+        it("divisible by fifteen", function () {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+  });
   describe("knows when a number is not", function() {
     it("divisible by three", function () {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
-    })
-  })
+    });
+    it("divisible by five", function () {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+    it("divisible by fifteen", function () {
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
+    });   
+  });  
+  
+  describe("when playing, says", function () {
+    it("'Java' when a number is divisible by three", function () {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+  });
 });
