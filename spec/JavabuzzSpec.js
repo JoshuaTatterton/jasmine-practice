04e51fc153f4e0e55f793edf3ac5db9f927e0844
Josh <Tattsy@hotmail.co.uk>
2015-08-17 12:00:32 +0100

    we now know fizz

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,15 @@
+describe("Javabuzz", function() {
+  var javabuzz;
+  describe("knows when a number is", function() {
+    it("divisible by three", function () {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    })
+  })
+  describe("knows when a number is not", function() {
+    it("divisible by three", function () {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    })
+  })
+});
